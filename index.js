0b4df967d198e38908dda43cefd40fe9b5de501a
JoshuaChris01 <joshuachris000@gmail.com>
2024-01-04 08:11:55 +0100

    New changes

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-const droplist = document.querySelectorAll('.drop__list select')
+const droplist = document.querySelectorAll('.drop__list select'),
+    fromCurrency = document.querySelector('.from select'),
+    toCurrency = document.querySelector('.to select'),
+    button = document.querySelector('.form button')
 
 for (let i = 0; i < droplist.length; i++) {
     for (const currencyCode in countryCode) {
-        let optionTag = ` <option value="${currencyCode}">${currencyCode}</option>`
+
+        let selected;
+        if (i === 0) {
+            selected = currencyCode == 'US' ? 'selected' : ''
+        } else if (i === 1) {
+            selected = currencyCode == 'NG' ? 'selected' : ''
+        }
+        let optionTag = `<option value="${currencyCode}" ${selected}>${currencyCode}</option>`
         droplist[i].insertAdjacentHTML('beforeend', optionTag)
     }
+}
 
+button.addEventListener('click', e => {
+    e.preventDefault();
+    getExchange();
+
+})
+const getExchange = () => {
+    const amount = document.querySelector('.form amount')
+    let amountValue = amount.value
+
+    if (amountValue === '' || amountValue === '0') {
+        amountValue = 1
+        amount.value = '1'
+    }
+    let url = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/ ${fromCurrency.value}`
+    
 }
