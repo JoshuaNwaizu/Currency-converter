7ae9c2a5f357304eec4c5b96e3b55abc72cc2de2
JoshuaChris01 <joshuachris000@gmail.com>
2024-01-06 09:07:53 +0100

    New changes

--- index.js
+++ index.js
@@ -15,21 +15,47 @@ for (let i = 0; i < droplist.length; i++) {
         let optionTag = `<option value="${currencyCode}" ${selected}>${currencyCode}</option>`
         droplist[i].insertAdjacentHTML('beforeend', optionTag)
     }
+    droplist[i].addEventListener('click', e => {
+        loadFlag(e.target)
+    })
 }
+
+function loadFlag(element) {
+   for( code in countryCode) {
+    if( code === element.value) {
+        let imgTag = element.parentElement.querySelector('img')
+        imgTag.src = `https://flagsapi.com/${countryCode[code]}/flat/64.png`
+    }
+   }
+}
+
 
 button.addEventListener('click', e => {
     e.preventDefault();
     getExchange();
 
 })
+const exchangeIcon = document.querySelector('.icon')
+exchangeIcon.addEventListener('click', () => {
+    let tempCode = fromCurrency.value
+    fromCurrency.value = toCurrency.value
+    toCurrency.value = tempCode
+    loadFlag(fromCurrency)
+    loadFlag(toCurrency)
+    getExchange()
+})
+
 const getExchange = () => {
     const amount = document.querySelector('.amount input')
+    const exchangeRateTxt = document.querySelector('.exchange__rate')
     let amountValue = amount.value
 
     if (amountValue === '' || amountValue === '0') {
         amount.value = '1'
         amountValue = 1
     }
+
+    exchangeRateTxt.innerText = 'Optimizing Exchange Rate'
     let apiKey = '613612c0b11dd1367eed7ce9'
     let url = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/${fromCurrency.value}`
     fetch(url)
@@ -37,8 +63,9 @@ const getExchange = () => {
     .then(result => {
         let exchangeRate = result.conversion_rates[toCurrency.value]
         let totalExchangeRate = (amountValue * exchangeRate).toFixed(2)
-        const exchangeRateTxt = document.querySelector('.exchange__rate')
         exchangeRateTxt.innerText = `${amountValue} ${fromCurrency.value} = ${totalExchangeRate} ${toCurrency.value}.`
+    }).catch(() => {
+        exchangeRateTxt.innerText = 'Something Went Wrong'
     })
     
 }
